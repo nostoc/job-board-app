2188f33817e27156aa296983f16843feede74ea1
nostoc <nostochk@gmail.com>
2026-04-22 16:26:45 +0530

    Remove db init script

--- init-dbs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE DATABASE jobs_db;
-	CREATE DATABASE payments_db;
-	CREATE DATABASE applications_db;
-EOSQL
